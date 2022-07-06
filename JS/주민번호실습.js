01a26e920d4e02d8edf7f31978cdd1314240352d
ju0ryu <0802ryu@gamil.com>
2022-07-06 17:53:00 +0900

    07/06 17:52

--- JS/주민번호실습.js
+++ JS/주민번호실습.js
@@ -1,19 +1,29 @@
-var personNum = prompt('주민등록번호를 입력하여 주세요');
-var number = personNum.replace('-','');
-var sum = 0;
-var weight = 2;
+function inputCheck() { 
+    // var personNum = prompt('주민등록번호를 입력하여 주세요');
+    // var number = personNum.replace('-','');
+    //첫번째 방법
+    // let personNum1 = document.inputForm.personNum1.value;
+    // let personNum2 = document.inputForm.personNum2.value;
+    //두번째 방법
+    let personNum1 = form.personNum1.value;
+    let personNum2 = form.personNum2.value;
 
-for(let i = 0; i<number.length-1; i++){
-        sum += number[i]*weight;
-        weight++;
-        if(weight==10){
-            weight=2;
+    var number = personNum1 + personNum2;
+    var sum = 0;
+    var weight = 2;
+    
+    for(let i = 0; i<number.length-1; i++){
+            sum += number[i]*weight;
+            weight++;
+            if(weight==10){
+                weight=2;
+            }
         }
-}
-
-var checkDigit = (11-(sum%11))%10;
-if(number[12] == checkDigit){
-    console.log(`${personNum}(O)`);
-}else{
-    console.log(`${personNum}(X)`);
+    
+    var checkDigit = (11-(sum%11))%10;
+    if(number[12] == checkDigit){
+        alert(`${number}(O)`);
+    }else{
+        alert(`${number}(X)`);
+        }
 }
