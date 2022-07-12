70d3d0a77be6fa361f0cc3253b8089bf25fcee73
ju0ryu <0802ryu@gamil.com>
2022-07-12 14:12:42 +0900

    07/12 14:12

---
+++ react/hello-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>리액트 안녕!</h1>
+      <h2>잘 작동하고있니?</h2>
+    </>
+  );
+}
+
+export default App;
