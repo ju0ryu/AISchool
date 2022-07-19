7fb24af89e224ffec4ef0119edcbece54f06e335
ju0ryu <0802ryu@gamil.com>
2022-07-19 16:55:32 +0900

    07/19 18:55

--- react/styleing-react/src/App.js
+++ react/styleing-react/src/App.js
@@ -3,12 +3,34 @@ import "./App.css";
 import { render } from "@testing-library/react";
 import SassComponent from "./SassComponent";
 import { Component } from "react";
+import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <SassComponent />
+//       </div>
+//     );
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <CSSModule />
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
