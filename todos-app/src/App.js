258ae92a568f5024e9b49218c036fd9340640372
ju0ryu <0802ryu@gamil.com>
2022-07-22 14:14:14 +0900

    11장 todo 예제

---
+++ todos-app/src/App.js
@@ -0,0 +1,74 @@
+import React, { useReducer, useState, useRef, useCallback } from "react";
+import TodoTemplate from "./components/TodoTemplate";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+}
+
+const App = () => {
+  // const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  // 고유 값으로 사용 될 id
+  // ref 를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    // setTodos((todos) => todos.concat(todo));
+    dispatch({ type: "INSERT", todo });
+    nextId.current += 1; // nextId 1 씩 더하기
+  }, []);
+
+  const onRemove = useCallback((id) => {
+    // setTodos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: "REMOVE", id });
+  }, []);
+
+  const onToggle = useCallback((id) => {
+    // setTodos((todos) =>
+    //   todos.map((todo) =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo
+    //   )
+    // );
+    dispatch({ type: "TOGGLE", id });
+  }, []);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
