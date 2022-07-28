3ef208979321291e2325e5bce8790a1b8b1c5018
ju0ryu <0802ryu@gamil.com>
2022-07-28 17:55:43 +0900

    네이버 3일차

--- react/react_naver_api/src/Layout.js
+++ react/react_naver_api/src/Layout.js
@@ -4,25 +4,21 @@ import Cafe from './Cafe';
 import Image from './Image';
 import Blog from './Blog';
 import News from './News';
+import Movie from './Movie';
 
 const Layout = ({ search, page }) => {
-  const [data, setData] = useState(null);
-  console.log('search ===================================' + search);
-  console.log('page =====================================' + page);
+  const [itemData, setItemData] = useState('');
   const NaverClienetId = '0_C0gO6RfZqeUrupKHsU';
   const NaverClientSecret = 'JocrPfZsxe';
   const category = page;
   const url = `v1/search/${category}?query=${search}`;
-  console.log(url);
 
   useEffect(() => {
-    console.log('check ==================================');
     const fetchData = async () => {
       try {
         const response = axios
           .get(url, {
             params: {
-              // query: { search },
               start: 1, //검색 시작 위치
               display: 10, // 가져올 이미지 갯수
               sort: 'sim', //정렬 유형 (유사도)
@@ -33,7 +29,7 @@ const Layout = ({ search, page }) => {
             },
           })
           .then((response) => {
-            setData(response.data.items);
+            setItemData(response.data.items);
           });
       } catch (e) {
         console.log(e);
@@ -42,55 +38,54 @@ const Layout = ({ search, page }) => {
     fetchData();
   }, [page]);
 
-  if (category === 'cafearticle') {
-    return (
-      <div>
-        {data.map((item) => (
-          <Cafe key={item.link} items={item} />
-        ))}
-      </div>
-    );
-  } else if (category === 'image') {
-    return (
-      <div>
-        {data.map((item) => (
-          <Image key={item.link} items={item} />
-        ))}
-      </div>
-    );
-  } else if (category === 'blog') {
-    return (
-      <div>
-        {data.map((item) => (
-          <Blog key={item.link} items={item} />
-        ))}
-      </div>
-    );
-  } else if (category === 'news') {
-    return (
-      <div>
-        {data.map((items) => (
-          <News key={items.link} items={items} />
-        ))}
-      </div>
-    );
-  }
-  // return (
-  //   <div>
-  //     {console.log('data : ' + data)}
-  //     {console.log('page : ' + category)}
-  //     {console.log(typeof category)}
-  //     {category === 'cafearticle'
-  //       ? data.map((items) => <Cafe key={items.link} items={items} />)
-  //       : category === 'image'
-  //       ? data.map((items) => <Image key={items.link} items={items} />)
-  //       : category === 'blog'
-  //       ? data.map((items) => <Blog key={items.link} items={items} />)
-  //       : category === 'news'
-  //       ? data.map((items) => <News key={items.link} items={items} />)
-  //       : null}
-  //   </div>
-  // );
+  // if (category === 'cafearticle') {
+  //   return (
+  //     <div>
+  //       {itemData.map((item) => (
+  //         <Cafe key={item.link} items={item} />
+  //       ))}
+  //     </div>
+  //   );
+  // } else if (category === 'image') {
+  //   return (
+  //     <div>
+  //       {itemData.map((item) => (
+  //         <Image key={item.link} items={item} />
+  //       ))}
+  //     </div>
+  //   );
+  // } else if (category === 'blog') {
+  //   return (
+  //     <div>
+  //       {itemData.map((item) => (
+  //         <Blog key={item.link} items={item} />
+  //       ))}
+  //     </div>
+  //   );
+  // } else if (category === 'news') {
+  //   return (
+  //     <div>
+  //       {itemData.map((item) => (
+  //         <News key={item.link} items={item} />
+  //       ))}
+  //     </div>
+  //   );
+  // }
+  return (
+    <div>
+      {category === 'cafearticle'
+        ? itemData.map((items) => <Cafe key={items.link} items={items} />)
+        : category === 'image'
+        ? itemData.map((items) => <Image key={items.link} items={items} />)
+        : category === 'blog'
+        ? itemData.map((items) => <Blog key={items.link} items={items} />)
+        : category === 'news'
+        ? itemData.map((items) => <News key={items.link} items={items} />)
+        : category === 'movie'
+        ? itemData.map((items) => <Movie key={items.link} items={items} />)
+        : null}
+    </div>
+  );
 };
 
 export default Layout;
