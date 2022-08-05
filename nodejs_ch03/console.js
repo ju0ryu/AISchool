aed4574fa05efca2323b24b8ad04b00455ad41e0
ju0ryu <0802ryu@gamil.com>
2022-08-05 14:54:44 +0900

    node 1

--- nodejs_ch03/console.js
+++ nodejs_ch03/console.js
@@ -8,7 +8,7 @@ const obj = {
     },
   },
 };
-console.time("전체 시간");
+console.time("전체시간");
 console.log("평범한 로그입니다 쉼표로 구분해 여러 값을 찍을 수 있습니다");
 console.log(string, number, boolean);
 console.error("에러 메세지는 console.error에 담아주세요");
