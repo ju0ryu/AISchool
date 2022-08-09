a6f65de6ec2cce0d0952642def26269fee050807
ju0ryu <0802ryu@gamil.com>
2022-08-09 13:18:12 +0900

    08/09 오전

--- learn-express/app.js
+++ learn-express/app.js
@@ -4,53 +4,105 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const dotenv = require("dotenv");
+const MySQLStore = require("express-mysql-session")(session);
+
+const options = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "123456",
+  database: "bbs",
+};
+
+var sessionStore = new MySQLStore(options);
 
 dotenv.config();
-const app = express(); //서버 생성
+const app = express(); //서버 생성s
 app.set("port", process.env.PORT || 3000); // 포트번호 설정
 
-app.use(morgan("dev"));
-app.use("/", express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
+// app.use(morgan("dev"));
+// app.use("/", express.static(path.join(__dirname, "public")));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.COOKIE_SECRET,
+//     store: sessionStore,
+//     cookie: {
+//       httpOnly: true,
+//       secure: false,
+//     },
+//     name: "session-cookie",
+//   })
+// );
+
+const multer = require("multer");
+const fs = require("fs");
+
+try {
+  fs.readdirSync("uploads");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads폴더를 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+const upload = multer({
+  Storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
     },
-    name: "session-cookie",
-  })
-);
-
-app.use((req, res, next) => {
-  console.log("모든요청에 다 실행됩니다");
-  next();
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
-app.get(
-  "/",
-  (req, res, next) => {
-    //   console.log(req.query);
-    //   const var1 = req.query.var1;
-    //   const var2 = req.query.var2;
-    //   res.send({ data1: var1, data2: var2 });
-    //   res.sendFile(path.join(__dirname, "/index.html"));
-    console.log("GET / 요청에서만 실행됩니다.");
-    next();
-  },
-  (req, res) => {
-    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
-  }
-);
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
 });
+
+// app.post("/upload", upload.single("image"), (req, res) => {
+//   console.log(req.file, req.body);
+//   res.send("ok");
+// });
+
+app.post("/upload", upload.array("many"), (req, res) => {
+  console.log(req.file, req.body);
+  res.send("ok");
+});
+
+// app.use((req, res, next) => {
+//   console.log("모든요청에 다 실행됩니다");
+//   sess = req.session;
+//   sess.username = "juyoung";
+//   console.log("req.session.username : ", req.session.username);
+//   console.log("req.sessionID : ", req.sessionID);
+//   console.log("req.session : ", req.session);
+//   next();
+// });
+// app.get(
+//   "/",
+//   (req, res, next) => {
+//     //   console.log(req.query);
+//     //   const var1 = req.query.var1;
+//     //   const var2 = req.query.var2;
+//     //   res.send({ data1: var1, data2: var2 });
+//     //   res.sendFile(path.join(__dirname, "/index.html"));
+//     console.log("GET / 요청에서만 실행됩니다.");
+//     next();
+//   },
+//   (req, res) => {
+//     throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
+//   }
+// );
+
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   res.status(500).send(err.message);
+// });
 
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
